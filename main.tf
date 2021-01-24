e6be587bfcee61d48cc2b906f744a64ed5fe7384
assareh <assareh@hashicorp.com>
2021-01-24 15:53:20 -0800

    removing router

--- main.tf
+++ main.tf
@@ -61,24 +61,24 @@ resource "vsphere_virtual_machine" "vm" {
   }
 }
 
-resource "vsphere_virtual_machine" "router" {
-  name             = "${var.environment}-${var.app_name}-router"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
+# resource "vsphere_virtual_machine" "router" {
+#   name             = "${var.environment}-${var.app_name}-router"
+#   resource_pool_id = data.vsphere_resource_pool.pool.id
+#   datastore_id     = data.vsphere_datastore.datastore.id
 
-  annotation = local.common_tags
-  num_cpus   = var.num_cpus
-  memory     = var.memory
+#   annotation = local.common_tags
+#   num_cpus   = var.num_cpus
+#   memory     = var.memory
 
-  network_interface {
-    network_id   = data.vsphere_network.network.id
-    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
-  }
+#   network_interface {
+#     network_id   = data.vsphere_network.network.id
+#     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+#   }
 
-  ovf_deploy {
-    remote_ovf_url       = "http://192.168.20.20:8000/csr1000v-universalk9.03.15.00.S.155-2.S-std.ova"
-    disk_provisioning    = "thin"
-    ip_protocol          = "IPV4"
-    ip_allocation_policy = "DHCP"
-  }
-}
+#   ovf_deploy {
+#     remote_ovf_url       = "http://192.168.20.20:8000/csr1000v-universalk9.03.15.00.S.155-2.S-std.ova"
+#     disk_provisioning    = "thin"
+#     ip_protocol          = "IPV4"
+#     ip_allocation_policy = "DHCP"
+#   }
+# }
--- outputs.tf
+++ outputs.tf
@@ -24,13 +24,13 @@ ssh ubuntu@${vsphere_virtual_machine.vm.default_ip_address}
 SSH
 }
 
-output "router_ip" {
-  value = vsphere_virtual_machine.router.default_ip_address
-}
+# output "router_ip" {
+#   value = vsphere_virtual_machine.router.default_ip_address
+# }
 
-output "router_ssh_addr" {
-  value = <<SSH
-Connect to your router via SSH:
-ssh ubuntu@${vsphere_virtual_machine.router.default_ip_address}
-SSH
-}
+# output "router_ssh_addr" {
+#   value = <<SSH
+# Connect to your router via SSH:
+# ssh ubuntu@${vsphere_virtual_machine.router.default_ip_address}
+# SSH
+# }
